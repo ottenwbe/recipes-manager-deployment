8339301dfeb9f517ba414f013c742bf5f3d77173
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2021-12-22 11:42:33 +0100

    updated chart for json config

--- minikube_start.sh
+++ minikube_start.sh
@@ -5,3 +5,8 @@ set -ex
 minikube start --driver podman
 minikube addons enable ingress
 minikube addons enable registry
+
+# Start the registry
+docker stop minikube-reg | true
+docker rm minikube-reg | true
+docker run --name "minikube-reg" -d --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
--- minikube_update.sh
+++ minikube_update.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-# Start the registry
-docker rm minikube-reg | true
-docker run --name "minikube-reg" -d --rm -it --network=host alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:$(minikube ip):5000"
-
 pushd .
 cd ../recipes-manager-ui
 ./scripts/make_docker_for_minikube.sh
